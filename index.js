754def1c26d08deeedd9dcf2de2dffc55b5f24d0
sabby155 <nebula155@gmail.com>
2019-04-23 16:59:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    const pTag = document.getElementById('text')
+    pTag.innerHTML = 'This is really cool!'
+})
+
