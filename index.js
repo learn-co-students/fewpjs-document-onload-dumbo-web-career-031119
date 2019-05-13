1ecfb1724fc138fc2a2466eede45b368380002fa
tpr1992 <terence.pataneronan@gmail.com>
2019-05-13 12:35:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+// // Your code goes here
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  let pTag = document.querySelector('p');
+  console.log(pTag)
+  pTag.innerHTML = "This is really cool!"
+  console.log(pTag.innerHTML)
+
+})
