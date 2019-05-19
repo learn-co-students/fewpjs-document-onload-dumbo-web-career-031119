f9f35aa433e6fc9e3bdb4ba7625d1e9155b8dce4
Jerry Velazquez <jvr572@gmail.com>
2019-05-19 18:49:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+let paragraph = document.querySelector('#text');
+
+document.addEventListener('DOMContentLoaded', function(e) {
+newDOM()
+console.log("the DOM has loaded");
+});
+
+const newDOM = () => {
+  paragraph.textContent = "This is really cool!"
+
+}
